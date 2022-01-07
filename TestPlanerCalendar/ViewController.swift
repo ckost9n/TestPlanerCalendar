db43f67b9403a84a590e2b88f79129e5ff286fe9
ckost9n <ckost9n@gmail.com>
2022-01-07 17:54:54 +0300

    Initial Commit

---
+++ TestPlanerCalendar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestPlanerCalendar
+//
+//  Created by Konstantin on 07.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
